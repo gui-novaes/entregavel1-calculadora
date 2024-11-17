4031aefeebf363a45af8b19c79690ffdad5f0c87
Guilherme <guilhermenovaes10.gnr@gmail.com>
2024-11-17 14:20:39 -0300

    Adicionando calculadora

---
+++ script.js
@@ -0,0 +1,56 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//ESCREVA SEU CÓDIGO AQUI!!
+
+function Calculadora(numero1, numero2, operador){
+  let resultado;
+  let num1 = parseInt(numero1)
+  let num2 = parseInt(numero2)
+  switch (operador) {
+    case "+":
+      resultado = num1 + num2
+      break;
+    case "-":
+      resultado = num1 - num2
+      break;
+    case "*":
+      resultado = num1 * num2
+      break;
+    case "/":
+      resultado = num1 / num2
+      break;
+    case "e":
+      resultado = num1 ** num2
+      break;
+  }
+  if (resultado == undefined || resultado > 1000000){
+    resultado = 'ERRO'
+  }
+  return resultado
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+let visor = document.getElementById("visor");
+
+function adicionar(tecla) {
+  visor.value = visor.value + tecla;
+}
+
+function limpezatotal() {
+  visor.value = "";
+}
+
+function calcular() {
+  let numeros = visor.value.split(/[^0-9]/)
+  let operadores = visor.value.split(/[0-9]/)
+  let operador
+  operadores.forEach(x => {
+    if (x != "")
+      operador = x
+  });
+
+  visor.value = Calculadora(numeros[0], numeros[1], operador)
+}
